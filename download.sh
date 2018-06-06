dfa2ee0e5b4ac0793f3798aeace79ef864307105
Ben Hero <ben@softninja.ca>
2018-06-05 23:46:51 -0400

    Commit missing proj, workspace, pod, sh, cartfile

---
+++ download.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+curl -L https://www.dropbox.com/s/wcilqcytjz97r4c/tinyYolo.mlmodel?dl=0 > camX/ObjectDetection/tinyYolo.mlmodel
+
+curl -L https://www.dropbox.com/s/workwx781attjyy/yolo2.mlmodel?dl=0 > camX/ObjectDetection/yolo2.mlmodel
